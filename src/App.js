cf64ae1a8e424ba11d6cca357137fc1cf9760d45
kyinn1307 <shruddls1307@gmail.com>
2024-10-14 03:08:12 +0900

    feat : create main page and set up routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+  </Routes>;
 }
 
 export default App;
