355d5eb03d950e74c53fe77e979c2e31c2c347cf
kyinn1307 <shruddls1307@gmail.com>
2024-10-21 16:55:27 +0900

    feat: add setScreen method for dynamic screen size

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,17 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+       --vh: 100%;
+    }
     body {
         display: flex;
         justify-content: center;
         margin : 0;
         font-family : "Pretendard";
-        height: 798px;
-        width: 393px;
         
         @media all and (max-width: 780px) { 
             width: 100vw;
-            height: 100dvh;
+            height: calc(var(--vh, 1vh) * 100);
         }
     }
 `;
